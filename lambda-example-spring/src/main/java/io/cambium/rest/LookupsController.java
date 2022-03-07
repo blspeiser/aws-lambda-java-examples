8244bacfbcebb3a27b669562c8c8728f2cab2ce3
Baruch Speiser <sbaruch@cambium.co.il>
2022-03-07 13:56:19 +0200

    Add lambda test and fix GET->POST because the request has a body

--- lambda-example-spring/src/main/java/io/cambium/rest/LookupsController.java
+++ lambda-example-spring/src/main/java/io/cambium/rest/LookupsController.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -31,7 +31,7 @@ public class LookupsController {
   @Autowired
   private LookupService service;
   
-  @GetMapping
+  @PostMapping
   public GetLookupsResponse getLookups(GetLookupsRequest request) {
     try {
       List<LookupData> lookups = service.lookup(request.type);
---
+++ lambda-example-spring/src/test/java/io/cambium/tests/LambdaTest.java
@@ -0,0 +1,88 @@
+package io.cambium.tests;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.ws.rs.HttpMethod;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import com.amazonaws.serverless.proxy.internal.LambdaContainerHandler;
+import com.amazonaws.serverless.proxy.internal.testutils.AwsProxyRequestBuilder;
+import com.amazonaws.serverless.proxy.internal.testutils.MockLambdaContext;
+import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
+import com.amazonaws.services.lambda.runtime.Context;
+
+import io.cambium.aws.StreamLambdaHandler;
+import io.cambium.types.requests.GetLookupsRequest;
+
+/**
+ * LambdaTest.
+ *
+ * @author com.amazonaws.serverless.archetypes:aws-serverless-springboot2-archetype:1.7
+ * @author Baruch Speiser, Cambium.
+ */
+public class LambdaTest {
+  private static StreamLambdaHandler handler;
+  private static Context lambdaContext;
+
+  @BeforeAll
+  public static void setUp() {
+    handler = new StreamLambdaHandler();
+    lambdaContext = new MockLambdaContext();
+  }
+
+  @Test
+  public void testLookups() {
+    InputStream requestIS = 
+        new AwsProxyRequestBuilder("/api/lookups", HttpMethod.POST)
+          .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
+          .body(new GetLookupsRequest("pets"))
+          .buildStream();
+    
+    
+    ByteArrayOutputStream responseOS = new ByteArrayOutputStream();
+    handle(requestIS, responseOS);
+
+    AwsProxyResponse response = readResponse(responseOS);
+    assertNotNull(response);
+    assertEquals(Response.Status.OK.getStatusCode(), response.getStatusCode());
+    assertFalse(response.isBase64Encoded());
+    assertTrue(response.getBody().contains("lookups"));
+    assertTrue(response.getBody().contains("[]"));
+
+    assertTrue(response.getMultiValueHeaders().containsKey(HttpHeaders.CONTENT_TYPE));
+    assertTrue(response.getMultiValueHeaders().getFirst(HttpHeaders.CONTENT_TYPE)
+                .startsWith(MediaType.APPLICATION_JSON));
+  }
+
+  private void handle(InputStream is, ByteArrayOutputStream os) {
+    try {
+      handler.handleRequest(is, os, lambdaContext);
+    } catch(IOException e) {
+      e.printStackTrace();
+      fail(e.getMessage());
+    }
+  }
+
+  private AwsProxyResponse readResponse(ByteArrayOutputStream responseStream) {
+    try {
+      return LambdaContainerHandler.getObjectMapper().readValue(responseStream.toByteArray(), AwsProxyResponse.class);
+    } catch(IOException e) {
+      e.printStackTrace();
+      fail("Error while parsing response: " + e.getMessage());
+    }
+    return null;
+  }
+}
